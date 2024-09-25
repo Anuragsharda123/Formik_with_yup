926d7ed14f3a6468bacbf1b6f86ca041a0628baa
Anuragsharda123 <anuragsharda131@gmail.com>
2024-09-25 23:55:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 function App() {
+
+  const validationSchema = yup.object().shape({
+    address:yup.array().of(
+      yup.object().shape({
+        title: yup.string()
+        .required("title field is required bro 🤟")
+        .matches(/^(?!.*\s{2,}).*$/, "Bhai itni space kya krega??"),
+        
+        street:yup.string()
+        .required("street field required bro 🤟")
+        .matches(/^(?!.*\s{2,}).*$/, "Bhai itni space kya krega??"),
+
+        city:yup.string()
+        .required("City field is required bro 🤟")
+        .matches(/^(?!.*\s{2,}).*$/, "Bhai itni space kya krega??"),
+
+        country:yup.string()
+        .required("country field is required bro 🤟")
+        .matches(/^(?!.*\s{2,}).*$/, "Bhai itni space kya krega??"),
+
+        pincode:yup.string()
+        .length(6,"Pincode should be of 6 digits")
+        .matches(/^(?!.*\s{2,}).*$/, "Bhai itni space kya krega??")
+        .required("Bhai pin code to de")
+      })
+    )
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='App'>
+
+    <Formik
+    initialValues={{
+      address:[{
+        title:'',
+        street:'',
+        city:'',
+        country:'',
+        pincode:''
+      }]
+    }}
+    validationSchema={validationSchema}
+    
+
+    >
+      {({values, setFieldValue, validateForm}) => (
+        <Form>
+          {values.address.map((_, index) => (
+            <div key={index}>
+              {['title','street', 'city', 'country', 'pincode'].map((field, i) => (
+                <div key={i}>
+                  <label> {field}: </label>
+                  <Field name={`address[${index}].${field}`} placeholder={`Enter ${field}`} />
+                  <ErrorMessage name={`address[${index}].${field}`} component="div" style={{ color: 'red' }} />
+                </div>
+              ))}
+
+              {values.address.length >1 && (
+                <button onClick={()=>{
+                  const updatedData = values.address.filter((_, arrindex) => arrindex !== index);
+                  setFieldValue('address', updatedData);
+                }}>
+                Delete
+              </button>
+              )}
+            </div>
+          ))}
+          <button type='button' onClick={async ()=>{
+            const errors = await validateForm();
+            if(Object.keys(errors).length ===0)
+            {
+              const newData = [...values.address, {title:'', street:'', city:'', country:'', pincode:''}];
+              setFieldValue('address', newData);
+            }
+            else{
+              alert("Validation errors", errors);
+            }
+
+          }}>Add address</button>
+        </Form>
+      )}
+
+    </Formik>
+
+
+      </div>
+    </>
   );
 }
 
